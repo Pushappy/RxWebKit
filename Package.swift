b1920340fc9d037179f2bef62d144bc9e82a622b
Hampus Iggström <hampus@iggstrom.com>
2021-06-17 11:12:12 +0200

    Update packages

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "RxWebKit",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v9)],
     products: [
         .library(name: "RxWebKit", targets: ["RxWebKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift", from: "5.0.1"),
+        .package(url: "https://github.com/ReactiveX/RxSwift", from: "6.0.0"),
         .package(url: "https://github.com/Quick/Quick.git", from: "2.2.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", .branch("master"))
+        .package(url: "https://github.com/Quick/Nimble.git", .branch("main"))
     ],
     targets: [
         .target(name: "RxWebKit", dependencies: ["RxSwift", "RxCocoa"], path: "RxWebKit"),
